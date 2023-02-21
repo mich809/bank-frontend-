0b00cf79d71d0d48bac5ace66fad1abaf5aac28a
Michael Caridad <caridadmichael@gmail.com>
2023-02-21 19:15:23 -0400

    fix mobile

--- bank-frontend/src/pages/Features.tsx
+++ bank-frontend/src/pages/Features.tsx
@@ -8,30 +8,30 @@ function Features() {
   return (
     <>
       {" "}
-      <div className="h-screen  flex flex-col items-center ">
-        <p className="text-white font-extrabold md:text-8xl w-[44rem] text-center ">
+      <div className="grid grid-col-1  md:flex md:flex-col md:items-center ">
+        <p className="text-white   font-extrabold md:text-8xl md:w-[44rem] text-center ">
           LEARN ABOUT OUR POWERS
         </p>
-        <div className="grid grid-rows-2 grid-cols-2 gap-4 md:w-10/12 md:mt-4 h-full ">
-          <div className="bg-[#ed732b]  rounded-3xl flex flex-col items-center justify-center h-[20rem]">
-            <img src={unified} alt="" />
+        <div className="grid grid-cols-1   md:grid-rows-2 md:grid-cols-2 gap-4 md:w-10/12 md:mt-4 md:h-full ">
+          <div className="bg-[#ed732b] rounded-3xl  mx-4  flex flex-col items-center justify-center h-[20rem]">
+            <img src={unified} alt="" className="" />
             <p className="text-white font-semibold text-2xl">
               Unified Payment System
             </p>
           </div>
-          <div className="bg-[#85ed2b]  rounded-3xl flex flex-col items-center justify-center h-[20rem]">
+          <div className="bg-[#85ed2b] rounded-3xl  mx-4   flex flex-col items-center justify-center h-[20rem]">
             <img src={automatic} alt="" />
             <p className="text-white font-semibold text-2xl">
               Automatic Bill Payments
             </p>
           </div>
-          <div className="bg-[#85ed2b] mt-6  rounded-3xl flex flex-col items-center justify-center h-[20rem]">
+          <div className="bg-[#ed732b] md:bg-[#85ed2b]  mx-4  md:mt-6 rounded-3xl flex flex-col items-center justify-center h-[20rem]">
             <img src={safeBox} alt="" />
             <p className="text-white font-semibold text-2xl">
               Secured Online Banking
             </p>
           </div>
-          <div className="bg-[#85ed2b] mt-6  rounded-3xl flex flex-col items-center justify-center h-[20rem]">
+          <div className="bg-[#85ed2b] md:mt-6 rounded-3xl  mx-4  flex flex-col items-center justify-center h-[20rem]">
             <img src={digital} alt="" className="" />
             <p className="text-white font-semibold text-2xl">
               Dedicated Digital Wallet
