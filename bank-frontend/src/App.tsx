eb423294d31087b447e7ca5bffcc1b60da4c0fe8
Michael Caridad <caridadmichael@gmail.com>
2023-02-21 19:00:37 -0400

    not much

--- bank-frontend/src/App.tsx
+++ bank-frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
+import Features from "./pages/Features";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <NavBar />
+      <Features />
+    </>
+  );
 }
 
 export default App;
